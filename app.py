4f5850448fe22b679cc5486f9634735060c3f771
Ronei Simoes <55517128+roneisimoes@users.noreply.github.com>
2022-05-06 20:25:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ st.write("# Classificação de Iris")
 st.write("## Exemplo com comprimento de petala e sépala")
 
 st.sidebar.write("## Parâmetros")
-st.sidebar.slider("Comprimento da sépala", 4, 8, 5.8, 0.1)
-st.sidebar.slider("Comprimento da pétala", 0.9, 7, 3.8, 0.1)
+st.sidebar.slider("Comprimento da sépala", 4.0, 8.0, 5.8, 0.1)
+st.sidebar.slider("Comprimento da pétala", 0.9, 7.0, 3.8, 0.1)
