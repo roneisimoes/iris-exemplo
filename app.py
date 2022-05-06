b4869c874b141faa1c3584ee67373857a4e89e7a
Ronei Simoes <55517128+roneisimoes@users.noreply.github.com>
2022-05-06 20:24:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,7 @@ import streamlit as st
 
 st.write("# Classificação de Iris")
 st.write("## Exemplo com comprimento de petala e sépala")
+
+st.sidebar.write("## Parâmetros")
+st.sidebar.slider("Comprimento da sépala", 4, 8, 5.8, 0.1)
+st.sidebar.slider("Comprimento da pétala", 0.9, 7, 3.8, 0.1)
