0f6a8e3bd1bb76e1eeff63d16b8e1d5858ba73a7
Ronei Simoes <55517128+roneisimoes@users.noreply.github.com>
2022-05-06 21:01:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ st.write("# Classificação de Iris")
 st.write("## Exemplo com comprimento de petala e sépala")
 
 st.sidebar.write("## Parâmetros")
-st.sidebar.slider("Comprimento da sépala", 4, 8, 5.8, 0.1)
-st.sidebar.slider("Comprimento da pétala", 0.9, 7, 3.8, 0.1)
+st.sidebar.slider("Comprimento da sépala", 4.0, 8.0, 5.8, 0.1)
+st.sidebar.slider("Comprimento da pétala", 0.9, 7.0, 3.8, 0.1)
 
 with open("objetos.pkl", "rb") as arquivo:
   ss, dtc = pickle.load(arquivo)
